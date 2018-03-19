019b6b37acd2cb4b587ed0b16ae58dc8e3a7e990
James Greig <3stripe@gmail.com>
2018-03-19 20:18:14 +0000

    First commit and barebones setup

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var livereload = require('gulp-livereload');
+
+gulp.task('compile', function () {
+    'use strict';
+    var twig = require('gulp-twig');
+    return gulp.src('./src/index.twig')
+        .pipe(twig({
+            data: {
+                title: 'Gulp and Twig',
+                benefits: [
+                    'Fast',
+                    'Flexible',
+                    'Free'
+                ]
+            }
+        }))
+        .pipe(gulp.dest('./web'))
+        .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('./src/**/*.twig', ['compile']);
+});
+
+gulp.task('default', ['watch']);
