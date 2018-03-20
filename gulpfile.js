583a26c29ef7de890dcc3704fcd0bf93c0dbd5e6
James Greig <3stripe@gmail.com>
2018-03-20 14:29:06 +0000

    Gulpfile cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var swig = require('gulp-swig');
 var data = require('gulp-data');
 var livereload = require('gulp-livereload');
+var twig = require('gulp-twig');
 var fs = require('fs');
 
 gulp.task('compile', function () {
     'use strict';
-    var twig = require('gulp-twig');
-    return gulp.src(['./src/*.twig', '!./src/_data.json', '!./src/layout.twig'])
+    return gulp.src(['./src/*.twig', '!./src/layout.twig'])
       .pipe(data(function(file) {
         return JSON.parse(fs.readFileSync('./src/_data.json'));
       }))
@@ -26,8 +26,7 @@ gulp.task('js', function () {
 
 gulp.task('watch', function() {
   livereload.listen();
-  gulp.watch('./src/**/*.twig', ['compile']);
-  gulp.watch('./src/_data.json', ['compile']);
+  gulp.watch(['./src/*/.twig','./src/_data.json'], ['compile']);
   gulp.watch('./src/**/*.js', ['js']);
 });
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 window.onblur = function() {
-  document.title = '🌚' + (document.title).substring(2);
+  document.title = '🌚 ' + (document.title).substring(2);
 }
 window.onfocus = function() {
-  document.title = '🌝' + (document.title).substring(2);
+  document.title = '🌝 ' + (document.title).substring(2);
 }
